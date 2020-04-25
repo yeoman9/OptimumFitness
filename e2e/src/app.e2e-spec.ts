d11a603d64b3ad0b77be8c3f5128a7ff572a722a
mayurpat <mayur.patel@efi.com>
2020-04-25 12:17:52 +0530

    missing files

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-8-jwt-authentication-example!');
+    expect(page.getTitleText()).toEqual('Welcome to OptimumFitness!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-8-jwt-authentication-example'),
+      dir: require('path').join(__dirname, './coverage/OptimumFitness'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
